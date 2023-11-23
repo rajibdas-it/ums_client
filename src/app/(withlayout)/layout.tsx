496d253e3ab29241635d50bd2769f6c4f8e24dc4
rajibdas-it <rajibrad@gmail.com>
2023-11-23 21:56:50 +0600

    create layout for project

---
+++ src/app/(withlayout)/layout.tsx
@@ -0,0 +1,14 @@
+import { Layout } from "antd";
+import Sidebar from "@/components/ui/Sidebar";
+import Contents from "@/components/ui/Contents";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Layout hasSider>
+      <Sidebar />
+      <Contents>{children}</Contents>
+    </Layout>
+  );
+};
+
+export default DashboardLayout;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>This is Login page</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
+<h1>This is Login page</h1>;
