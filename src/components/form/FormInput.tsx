0ef7657bdbbf2cb201eb8bbcfa029e2c778f3274
rajibdas-it <rajibrad@gmail.com>
2023-11-24 01:55:09 +0600

    create reusable form

---
+++ src/components/form/FormInput.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Input } from "antd";
+import { useFormContext, Controller } from "react-hook-form";
+
+interface IInput {
+  name: string;
+  label?: string;
+  type?: string;
+  size?: "large" | "small";
+  value?: string | string[] | undefined;
+  id?: string;
+  placeholder?: string;
+  validation: Record<string, unknown>;
+}
+
+const FormInput = ({
+  name,
+  label,
+  type,
+  size,
+  value,
+  id,
+  placeholder,
+  validation,
+}: IInput) => {
+  const { control } = useFormContext();
+
+  return (
+    <>
+      {label ? label : null}
+      <Controller
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <Input
+            type={type}
+            size={size}
+            placeholder={placeholder}
+            {...field}
+            value={value ? value : field.value}
+            id={id}
+          />
+        )}
+      />
+    </>
+  );
+};
+
+export default FormInput;
---
+++ src/components/form/From.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { ReactElement, ReactNode } from "react";
+import {
+  useForm,
+  FormProvider,
+  useFormContext,
+  SubmitHandler,
+} from "react-hook-form";
+
+type FormConfig = {
+  defaultValues?: Record<string, any>;
+};
+
+type FormProps = {
+  children: ReactElement | ReactNode;
+  submitHandler: SubmitHandler<any>;
+} & FormConfig;
+
+const From = ({ children, submitHandler, defaultValues }: FormProps) => {
+  const fromConfig: FormConfig = {};
+
+  if (!!defaultValues) fromConfig["defaultValues"] = defaultValues;
+
+  const methods = useForm<FormProps>(fromConfig);
+  const { handleSubmit, reset } = methods;
+
+  const onSubmit = (data: any) => {
+    submitHandler(data);
+    reset();
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}></form>
+    </FormProvider>
+  );
+};
+
+export default From;
