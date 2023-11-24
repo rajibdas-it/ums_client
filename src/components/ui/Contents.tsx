4f449947f4d4a324f7a400499afff7039e8304aa
rajibdas-it <rajibrad@gmail.com>
2023-11-24 20:07:02 +0600

    implement logout function

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Layout } from "antd";
+import Headers from "./Headers";
 const { Content } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Content style={{ minHeight: "100vh", color: "black" }}>{children}</Content>
+    <Content style={{ minHeight: "100vh", color: "black" }}>
+      <Headers />
+      {children}
+    </Content>
   );
 };
 
---
+++ src/components/ui/Headers.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { Avatar, Dropdown, Layout, Row, Space } from "antd";
+import { Button, MenuProps } from "antd";
+import { UserOutlined, LoginOutlined } from "@ant-design/icons";
+import { getUserInfo, removeUserInfo } from "@/services/auth.service";
+import { authKey } from "@/constants/storage";
+import { useRouter } from "next/navigation";
+const { Header } = Layout;
+
+const Headers = () => {
+  const user: any = getUserInfo();
+
+  const router = useRouter();
+  const logOut = () => {
+    removeUserInfo(authKey);
+    router.push("/login");
+  };
+  const items: MenuProps["items"] = [
+    {
+      key: "0",
+      label: (
+        <Button onClick={logOut} type="text" danger>
+          {" "}
+          Logout <LoginOutlined />
+        </Button>
+      ),
+    },
+    {
+      key: "1",
+      label: <p>ID:{user?.id}</p>,
+    },
+  ];
+
+  return (
+    <Header>
+      <Row justify="end" align="middle" style={{ height: "100%" }}>
+        <Dropdown menu={{ items }}>
+          <a href="">
+            <Space wrap size={16}>
+              <Avatar
+                style={{ border: "1px solid white" }}
+                size="large"
+                icon={<UserOutlined />}
+              />
+            </Space>
+          </a>
+        </Dropdown>
+      </Row>
+    </Header>
+  );
+};
+
+export default Headers;
